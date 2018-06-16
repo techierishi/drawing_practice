2fcc5a28d36af26965f30e561b072f764357e16a
techierishi <techierishi@gmail.com>
2018-06-17 00:46:12 +0530

    Only link and filename required now

--- image_download.py
+++ image_download.py
@@ -1,35 +1,45 @@
 import urllib.request
 from selenium import webdriver
 
+def getAllImgLinks():
+	mainURL = 'https://www.drawingtutorials101.com/Anime-and-Manga-drawing-tutorials'
+	browser = webdriver.Firefox(executable_path="/home/techierishi/selenium_drivers/geckodriver")
+	browser.get(mainURL)
 
-allAnchorTexts = ["American Sparrows", "Amphibians", "Animal Faces", "Antarctic Animals", "Aquatic Mammals", "Arachnids", "Bears", "Beetles", "Big Cats", "Bird of prey", "Birds", "Butterflies", "Cats", "Chipmunks", "Corals", "Dogs", "Extinct Animals", "Farm Animals", "Fishes", "Horses", "Insects", "Jellyfishes", "Kangaroos", "Lemurs", "Lizards", "Marine Mammals", "Mollusks", "Other Animals", "Owls", "Parrots", "Primates", "Reptiles", "Rodents", "Scorpions", "Sea Water Animals", "Seabirds", "Seals", "Sharks", "Shorebirds", "Snails", "Snakes", "Squids", "Starfishes", "Turtles and Tortoises", "Wild Animals", "Woodpeckers", "Worms", "Zoo Animals"]
-browser = webdriver.Firefox(executable_path="/home/techierishi/selenium_drivers/geckodriver")
+	allAnchorTexts = browser.find_elements_by_class_name("block_category")
+	allAnchorTextsLinks = []
 
-# myfile = open('xyz.txt', 'w')
+	for linkToKeep in allAnchorTexts:
+		linkTextPair = {}
+		tutUrl = linkToKeep.get_attribute("href")
+		tutText = linkToKeep.text
+		linkTextPair["link"] = tutUrl
+		linkTextPair["text"] = tutText
+		allAnchorTextsLinks.append(linkTextPair)
+	
+	# print('allAnchorTextsLinks',allAnchorTextsLinks)
+	myfile = open('Animes.html', 'w')
 
-for textToClick in allAnchorTexts:
-	browser.get('https://www.drawingtutorials101.com/Animals-drawing-tutorials')
-	print(textToClick)
-	anchorToClick = browser.find_element_by_xpath("//*[contains(text(), '"+textToClick+"')]")
-	anchorToClick.click()
+	for linkToGo in allAnchorTextsLinks:
+		browser.get(linkToGo["link"])
+		print(linkToGo["text"])
+		tutorialsToClick = browser.find_elements_by_xpath("//*[contains(text(), 'View this Tutorial')]")
+		tutorialLinks = []
 
-	tutorialsToClick = browser.find_elements_by_xpath("//*[contains(text(), 'View this Tutorial')]")
-	tutorialLinks = []
+		for tutorialToClick in tutorialsToClick:
+			tutUrl = tutorialToClick.get_attribute("href")
+			tutorialLinks.append(tutUrl)
 
-	for tutorialToClick in tutorialsToClick:
-		tutUrl = tutorialToClick.get_attribute("href")
-		tutorialLinks.append(tutUrl)
+		# print('tutorialLinks',tutorialLinks)
+		count = 1
+		for tutUrl in tutorialLinks:
+			# print('tutUrl',tutUrl)
+			browser.get(tutUrl)
+			image = browser.find_elements_by_tag_name("img")
+			img_src = image[2].get_attribute("src")
+			print(img_src)
+			myfile.writelines('<a href="'+img_src+'"> '+linkToGo["text"]+' '+str(count)+' </a></br>')
+			count +=1
+	myfile.close()
 
-	# print('tutorialLinks',tutorialLinks)
-
-	for tutUrl in tutorialLinks:
-		# print('tutUrl',tutUrl)
-		browser.get(tutUrl)
-		# tutorialToClick.click()
-		image = browser.find_elements_by_tag_name("img")
-		img_src = image[2].get_attribute("src")
-		print(img_src);
-		# myfile.writelines(img_src)
-# download the image
-# urllib.request.urlretrieve(img_src, "captcha.png")
-# myfile.close()
+getAllImgLinks()
