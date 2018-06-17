f944a0b87d882a6ca52677e77f2a79e3c245b446
techierishi <techierishi@gmail.com>
2018-06-17 09:38:21 +0530

    Covered pagination as well

--- image_download.py
+++ image_download.py
@@ -16,30 +16,49 @@ def getAllImgLinks():
 		linkTextPair["link"] = tutUrl
 		linkTextPair["text"] = tutText
 		allAnchorTextsLinks.append(linkTextPair)
+
+	goToTutorialSublist(allAnchorTextsLinks,browser)
 	
 	# print('allAnchorTextsLinks',allAnchorTextsLinks)
+def goToTutorialSublist(allAnchorTextsLinks, browser):
 	myfile = open('Animes.html', 'w')
 
 	for linkToGo in allAnchorTextsLinks:
 		browser.get(linkToGo["link"])
 		print(linkToGo["text"])
-		tutorialsToClick = browser.find_elements_by_xpath("//*[contains(text(), 'View this Tutorial')]")
-		tutorialLinks = []
+		paginationUl = browser.find_element_by_class_name("pagination")
+		allPages = paginationUl.find_elements_by_tag_name("a")
+		allPaginationSet = set()
+		for pagi in allPages:
+			paginationLink= pagi.get_attribute('href')
+			if(paginationLink.endswith('#') != True):
+				allPaginationSet.add(paginationLink)
+		allPagination = list(allPaginationSet)
+		allPagination.sort()
+		print('allPagination',allPagination)
 
-		for tutorialToClick in tutorialsToClick:
-			tutUrl = tutorialToClick.get_attribute("href")
-			tutorialLinks.append(tutUrl)
+		for currentPage in allPagination:
+			print('currentPage',currentPage)
+			browser.get(currentPage)
+			tutorialsToClick = browser.find_elements_by_xpath("//*[contains(text(), 'View this Tutorial')]")
+			tutorialLinks = []
+			for tutorialToClick in tutorialsToClick:
+				tutUrl = tutorialToClick.get_attribute("href")
+				tutorialLinks.append(tutUrl)
+			gotToTutorial(tutorialLinks,browser,linkToGo,myfile)
 
-		# print('tutorialLinks',tutorialLinks)
-		count = 1
-		for tutUrl in tutorialLinks:
-			# print('tutUrl',tutUrl)
-			browser.get(tutUrl)
-			image = browser.find_elements_by_tag_name("img")
-			img_src = image[2].get_attribute("src")
-			print(img_src)
-			myfile.writelines('<a href="'+img_src+'"> '+linkToGo["text"]+' '+str(count)+' </a></br>')
-			count +=1
 	myfile.close()
+
+def gotToTutorial(tutorialLinks,browser,linkToGo,myfile):
+	# print('tutorialLinks',tutorialLinks)
+	count = 1
+	for tutUrl in tutorialLinks:
+		# print('tutUrl',tutUrl)
+		browser.get(tutUrl)
+		image = browser.find_elements_by_tag_name("img")
+		img_src = image[2].get_attribute("src")
+		print(img_src)
+		myfile.writelines('<a href="'+img_src+'"> '+linkToGo["text"]+' '+str(count)+' </a></br>')
+		count +=1
 
 getAllImgLinks()
